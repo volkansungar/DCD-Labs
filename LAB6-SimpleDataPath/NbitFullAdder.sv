0a2964135f1fe9be9ce4d55880660519882f5bb3
Volkan Sungar <111195273+volkansungar@users.noreply.github.com>
2025-05-12 13:45:52 +0300

    Update NbitFullAdder.sv

--- LAB6-SimpleDataPath/NbitFullAdder.sv
+++ LAB6-SimpleDataPath/NbitFullAdder.sv
@@ -1,13 +1,24 @@
-module InstrMem (
-  input logic clk,
-  output logic [13:0] instr);
-  
-  logic [13:0] mem_arr [3:0] = {14'b00000000011111, 14'b01111100001110, 14'b10111111101101, 14'b11110111111011};
-  integer PC = 0;
-  
-  always @(posedge clk) begin
-    instr <= mem_arr[PC];
-    PC <= PC + 1;
-  end
-  
+`include "fulladder.sv"
+module NbitFullAdder #(parameter N = 32) (
+    input logic [N-1:0] a, b,
+    input logic cin,
+    output logic [N-1:0] s,
+    output logic cout
+);
+
+    wire [N-2:0] c;
+
+    genvar i;
+    generate
+        for (i = 0; i < N; i++) begin : fulladder_instance
+            if (i == 0) begin
+                fulladder i_fa (a[i], b[i], cin, s[i], c[i]);
+            end else if (i == N - 1) begin
+                fulladder i_fa (a[i], b[i], c[i-1], s[i], cout);
+            end else begin
+                fulladder i_fa (a[i], b[i], c[i-1], s[i], c[i]);
+            end
+        end
+    endgenerate
+
 endmodule
